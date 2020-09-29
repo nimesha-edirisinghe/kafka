923260486ce374020cea1a9f740160da565f249c
nimesha-edirisinghe <nimesha@linearsquared.com>
2020-09-29 18:20:27 +0530

    add producer

---
+++ producer.js
@@ -0,0 +1,31 @@
+const { Kafka } = require("kafkajs");
+
+run();
+async function run() {
+  try {
+    const kafka = new Kafka({
+      clientId: "nimapp",
+      brokers: ["nimeshapc:9092"],
+      authenticationTimeout: 8000,
+    });
+    const producer = kafka.producer();
+    console.log("Connecting...");
+    await producer.connect();
+    console.log("Connected!");
+    //A-M, N-Z
+    await producer.createTopics({
+      topics: [
+        {
+          topic: "Producer",
+          numPartitions: 2,
+        },
+      ],
+    });
+    console.log("Created Successfully!");
+    await admin.disconnect();
+  } catch (ex) {
+    console.log(`Something bad happened ${ex}`);
+  } finally {
+    process.exit();
+  }
+}
